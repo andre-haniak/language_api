0f788a54db8502b049afcf162cb5cf0237345c50
André Dantas <43660680+andre-haniak@users.noreply.github.com>
2023-03-30 22:34:35 -0300

    initial commit

---
+++ src/test/java/com/language_api/language_api/LanguageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.language_api.language_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
